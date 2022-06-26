3ea82e85ecacadb243c4e1bebcd0c158b9d54333
Melissa Deven <99757260+Deven1991@users.noreply.github.com>
2022-06-26 13:13:51 -0500

    deleted node modules and package lock file

--- README.md
+++ README.md
@@ -15,8 +15,10 @@ If your README is long, add a table of contents to make it easy for users to fin
 
 - [Installation](#installation)
 - [Usage](#usage)
-- [Credits](#credits)
-- [License](#license)
+- [Purpose](#purpose)
+- [Features](#features)
+- [Demo](#demo)
+- [Questions](#questions)
 
 ## Installation
 
@@ -24,15 +26,13 @@ What are the steps required to install your project? Provide a step-by-step desc
 
 ## Usage
 
-Provide instructions and examples for use. Include screenshots as needed.
-
-To add a screenshot, create an `assets/images` folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
+First, clone the repository. Seecond, run npm install on the command line. Run mysql -u root -p to open the mysql shell. Run source db/schema.sql and source db/seeds.sql to create and seed a database. Type exit to leave the mysql shell. Lastly, run node index to start the command-line application.
 
     ```md
     ![alt text](assets/images/screenshot.png)
     ```
 
-## Credits
+## Purpose
 
 List your collaborators, if any, with links to their GitHub profiles.
 
@@ -40,7 +40,7 @@ If you used any third-party assets that require attribution, list the creators w
 
 If you followed tutorials, include links to those here as well.
 
-## License
+## Features
 
 The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
 
@@ -48,20 +48,12 @@ The last section of a high-quality README file is the license. This lets other d
 
 🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
 
-## Badges
+## Demo
 
 ![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
 
 Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
 
-## Features
+## Questions
 
 If your project has a lot of features, list them here.
-
-## How to Contribute
-
-If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
-
-## Tests
-
-Go the extra mile and write tests for your application. Then provide examples on how to run them here.
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const inquirer = require("inquirer");
-const cTable = require("console.table");
-const db = require("./db/connection");
+const consoleTable = require("console.table");
+const db = require('./db/connection');
 const { allowedNodeEnvironmentFlags, exit, listenerCount } = require('process');
 
 const startTrack = () => {
@@ -26,56 +26,208 @@ const firstQuestion = () => {
             name:'startChoice',
             message: 'What would you like to do?',
             choices:[
-                'View All Employees',
-                'View Roles',
-                'View Departments',
-                'View Employees by Department',
-                'Add Employee',
+                'Update Employee Role',
+                'View All Roles',
                 'Add Role',
+                'View All Departments',
                 'Add Department',
-                "Update Employee's Role",
+                'View All Employees',
                 'Exit'
             ]
         }
     ]).then((answer) => {
         switch (answer.startChoice) {
-            case 'View All Employees':
-                viewAllEmployees();
+            case 'Update Employee Role':
+                updateRole();
                 break;
-            case 'View Roles':
+            case 'View All Roles':
                 viewRoles();
-                break;
-            case 'View Departments':
-                viewDepartments();
-                break;
-            case 'View Employees by Department':
-                viewEmployeesByDepartment();
-                break;
-            case 'Add Employee':
-                addEmployee();
                 break;
             case 'Add Role':
                 addRole();
                 break;
+            case 'View All Departments':
+                viewDepartments();
+                break;
             case 'Add Department':
                 addDepartment();
                 break;
-            case "Update Employee's Role":
-                updateRole();
+            case 'View All Employees':
+                viewAllEmployees();
                 break;
             case 'Exit':
                 exit();
-                
-
         }
     })
 };
 
+const updateRole = () => {
+    inquirer.prompt([
+        {
+            type: 'list',
+            message: 'Which employee do you want to update?',
+            choices: employeesArray,
+            name: 'employeeUpdate'
+        },
+        {
+            type: 'list',
+            message: 'Which new role do you want to add for the employee?',
+            choices: rolesArray,
+            name: 'roleUpdate'
+        }
+    ]).then((answers) => {
+        const query = `SELECT id FROM roles WHERE title = ?`;
+        db.query(query, answers.roleUpdate, (err, result) => {
+            if (err) {
+                throw err;
+            }
+            const newId = result[0].id;  
+            const query2 = `UPDATE employees SET role_id = ? where first_name = ?`
+            db.query(query2, [newId, answers.employeeUpdate], (err, result) => {
+                if (err) {
+                    throw err
+                }
+                console.log("Employee's Role has been updated")
+                console.log('===============================================');
+                firstQuestion();
+            })
+        })
+    })
+} 
 
+const viewRoles = () => {
+    const query = `SELECT roles.id, roles.title AS Title, roles.salary AS Salary, departments.name AS Department FROM roles LEFT JOIN departments on roles.department_id = departments.id`
+    db.query(query, (err, res) => {
+        if (err) {
+            throw err
+        }
+        console.log('Viewing all roles');
+        console.log('===============================================');
+        console.table(res);
+        console.log('===============================================');
+        firstQuestion();
+    })
+};
 
+const addRole = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: 'What is the title of the role you want to add?',
+            name: 'newRole'
+        },
+        {
+            type: 'input',
+            message: 'What is the salary of this role?',
+            name: 'salary'
+        },
+        {
+            type: 'input',
+            message: 'What is the department ID number for this role?',
+            name: 'dept_id'
+        }
+    ]).then((answers) => {
+        const query = `INSERT INTO roles (title, salary, department_id) VALUES (?, ?, ?)`
+        db.query(query, [answers.newRole, answers.salary, answers.dept_id], 
+            (err) => {
+                if (err) {
+                    throw err;
+                }
+                rolesArray.push(answers.newRole)
+                console.log('Added new Role');
+                console.log('===============================================');
+                console.table(answers);
+                console.log('===============================================');
+                firstQuestion();
+            })
+    })
+}
 
+const viewDepartments = () => {
+    query = `SELECT departments.id, departments.name AS Department FROM departments`
+    db.query(query, (err, res) => {
+        if (err) {
+            throw err
+        }
+        console.log('Viewing all departments');
+        console.log('===============================================');
+        console.table(res);
+        console.log('===============================================');
+        firstQuestion();
+    })
+};
 
+const addDepartment = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: 'What department do you want to add?',
+            name: 'newDepartment'
+        }
+    ]).then((answer) => {
+        const query = `INSERT INTO departments (name) VALUES (?)`
+        db.query(query, answer.newDepartment, 
+        (err) => {
+            if (err) {
+                throw err;
+            }
+            departmentsArray.push(answer.newDepartment)
+            console.log('Added new Department');
+            console.log('===============================================');
+            console.table(answer);
+            console.log('===============================================');
+            firstQuestion();
+        })
+    })
+}
 
+const viewAllEmployees = () => {
+    const query = `SELECT employees.id, employees.first_name AS First, employees.last_name AS Last, roles.title AS Title, departments.name AS Department, roles.salary AS Salary, CONCAT(manager.first_name,' ', manager.last_name) AS Manager
+    FROM employees
+    LEFT JOIN employees manager on manager.id = employees.manager_id
+    INNER JOIN roles ON employees.role_id = roles.id
+    INNER JOIN departments ON departments.id = roles.department_id;`
+    db.query(query, (err, res) => {
+        if (err) {
+            throw err
+        }
+        console.log('Viewing all employees');
+        console.log('===============================================');
+        console.table(res);
+        console.log('===============================================');
+        firstQuestion();
+    })
+};
 
+const employeesArray = [];
+    const queryEmployees = `SELECT first_name FROM employees`
+    db.query(queryEmployees, (err, res) => {
+        if (err) {
+            throw err
+        }
+        res.forEach(({first_name}) => {
+            employeesArray.push(first_name);
+        });
+    });
+const rolesArray = [];
+    const queryRoles = `SELECT title FROM roles`
+    db.query(queryRoles, (err, res) => {
+        if (err) {
+            throw err
+        }
+        res.forEach(({title}) => {
+            rolesArray.push(title);
+        })
+    })
+const departmentsArray = [];
+    const queryDepartments = `SELECT name from departments`
+    db.query(queryDepartments, (err, res) => {
+        if (err) {
+            throw err
+        }
+        res.forEach(({name}) => {
+            departmentsArray.push(name);
+        })
+    })
 
 startTrack();

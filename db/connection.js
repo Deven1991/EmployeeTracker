fa36d3e2f44dd8bf95836983879c89651e469218
Melissa Deven <99757260+Deven1991@users.noreply.github.com>
2022-06-26 10:44:01 -0500

    added required three tables in schema

---
+++ db/connection.js
@@ -0,0 +1,21 @@
+const inquirer = require("inquirer");
+const cTable = require("console.table");
+const mysql = require("mysql2");
+const db = require("./db/connection");
+const { allowedNodeEnvironmentFlags, exit, listenerCount } = require('process');
+
+const startTrack = () => {
+    console.log("Welcome to Employee Tracker!");
+    console.log("|==============================================|");
+    console.log("|   ____                                       |");  
+    console.log("|  |       _   _   _      __        __  __     |");   
+    console.log("|  |---   | | | | |D)|   |  | |__| |_  |_      |");  
+    console.log("|  |____  |  V  | |  |__ |__|   |  |__ |__     |");  
+    console.log("|                                              |");  
+    console.log("|  _______                                     |");  
+    console.log("|     |     _    _    __        __   _         |");  
+    console.log("|     |    |D)  (_)  |    |_/  |_   |D)        |");  
+    console.log("|     |    | | |   | |__  | |  |__  | |        |");  
+    console.log("|==============================================|");
+    firstQuestion();
+};
